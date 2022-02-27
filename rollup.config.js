6db0979e7d3169c4b33a6046165dbd2f46334f0a
Julian Soto <juliansotoponce@gmail.com>
2022-02-26 22:53:40 -0800

    create rollup config file

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,25 @@
+import { DEFAULT_EXTENSIONS } from "@babel/core";
+import { babel } from "@rollup/plugin-babel";
+import packageJson from "./package.json";
+
 export default {
-  input: "src/index.js",
-  output: {
-    file: "dist/index.js",
-    format: "es",
-  },
+  input: "src/index.tsx",
+  output: [
+    {
+      file: packageJson.main,
+      format: "cjs",
+    },
+    {
+      file: packageJson.module,
+      format: "es",
+    },
+  ],
+  plugins: [
+    babel({
+      extensions: [...DEFAULT_EXTENSIONS, ".ts", ".tsx"],
+      babelHelpers: "runtime",
+      plugins: ["@babel/plugin-transform-runtime"],
+      presets: ["solid", "@babel/preset-typescript"],
+    }),
+  ],
 };
