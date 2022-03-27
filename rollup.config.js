702dbfa0aa9ba774cc34c30599a963a32e8f0617
Julian Soto <juliansotoponce@gmail.com>
2022-03-26 17:02:22 -0700

    fix: use rollup-plugin-typescript for generating types

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,8 @@ import commonjs from "@rollup/plugin-commonjs";
 import nodeResolve from "@rollup/plugin-node-resolve";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import postcss from "rollup-plugin-postcss";
+import typescript from "@rollup/plugin-typescript";
+import jsx from "acorn-jsx";
 import sass from "sass";
 import path from "path";
 
@@ -17,6 +19,7 @@ export default {
       preserveModules: true,
     },
   ],
+  acornInjectPlugins: [jsx()],
   plugins: [
     peerDepsExternal(),
     nodeResolve({
@@ -42,11 +45,12 @@ export default {
         },
       ],
     }),
+    typescript({ outDir: "./dist/src", exclude: ["./examples/**/*"] }),
     babel({
       extensions: [...DEFAULT_EXTENSIONS, ".ts", ".tsx"],
       babelHelpers: "runtime",
       plugins: ["@babel/plugin-transform-runtime"],
-      presets: ["solid", "@babel/preset-typescript"],
+      presets: ["solid"],
     }),
   ],
 };
