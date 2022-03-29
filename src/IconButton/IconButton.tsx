33259ea9ea7b837e106b4e8cc20f7ef376d6e986
Julian Soto <juliansotoponce@gmail.com>
2022-03-29 00:59:14 -0700

    feat(IconButton): set action-item classname if button is inside top app bar section

--- src/IconButton/IconButton.tsx
+++ src/IconButton/IconButton.tsx
@@ -26,13 +26,28 @@ const IconButton = (props: PropsWithChildren<IconButtonProps>) => {
   let buttonElement: HTMLButtonElement;
   const [buttonRipple, setButtonRipple] = createSignal<MDCRipple>();
   const [isNavIcon, setIsNavIcon] = createSignal(false);
+  const [isActionItem, setIsActionItem] = createSignal(false);
 
   createEffect(() => {
     const ripple = new MDCRipple(buttonElement);
     ripple.initialize();
     ripple.unbounded = true;
     setButtonRipple(ripple);
-    setIsNavIcon(Boolean(buttonElement.closest(".mdc-top-app-bar")));
+    // true if button is inside top app bar section
+    setIsNavIcon(
+      Boolean(
+        buttonElement.closest(
+          ".mdc-top-app-bar .mdc-top-app-bar__section.mdc-top-app-bar__section--align-start"
+        )
+      )
+    );
+    setIsActionItem(
+      Boolean(
+        buttonElement.closest(
+          ".mdc-top-app-bar .mdc-top-app-bar__section.mdc-top-app-bar__section--align-end"
+        )
+      )
+    );
   });
 
   onCleanup(() => buttonRipple()?.destroy());
@@ -47,6 +62,7 @@ const IconButton = (props: PropsWithChildren<IconButtonProps>) => {
           "mdc-icon-button": true,
           "mdc-icon-button--touch": true,
           "mdc-top-app-bar__navigation-icon": isNavIcon(),
+          "mdc-top-app-bar__action-item": isActionItem(),
         })}
         ref={(el) => (buttonElement = el)}
         aria-label={props.aria?.label}
